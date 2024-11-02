e8cb1af5a77615551a777606cbdeca1dcaa14a5a
yoshioka-s <yoshioka63@gmail.com>
2024-11-03 07:38:50 +0900

    feat: install lazy git

--- init.lua
+++ init.lua
@@ -260,6 +260,26 @@ require('lazy').setup({
       {'nvim-treesitter/nvim-treesitter'},
     },
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>c", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
   'folke/tokyonight.nvim',
   'christoomey/vim-tmux-navigator',
   'kien/ctrlp.vim',
